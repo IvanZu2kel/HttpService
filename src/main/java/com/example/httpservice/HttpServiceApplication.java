549d6bfe5cf2060900241c5701ff32ab71d129ec
King <ivan_zukkel@mail.ru>
2022-01-20 14:13:45 +0300

    Initial commit

---
+++ src/main/java/com/example/httpservice/HttpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.httpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpServiceApplication.class, args);
+    }
+
+}
